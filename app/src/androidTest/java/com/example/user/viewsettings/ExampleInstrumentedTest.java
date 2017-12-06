18ad6825f08feaa9eea9d63750825173f19aaf1e
Dima-1 <softmail32@gmail.com>
2017-12-06 22:49:44 +0300

    test FontSizeListPreference 100%

--- app/src/androidTest/java/com/example/user/viewsettings/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/user/viewsettings/ExampleInstrumentedTest.java
@@ -2,6 +2,7 @@ package com.example.user.viewsettings;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -9,10 +10,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
@@ -43,6 +52,19 @@ public class ExampleInstrumentedTest {
     public void testGeneralDisplayed() {
         testViewSettingsDisplayed();
         onView(withText(R.string.pref_title_font_size)).check(matches(isDisplayed()));
-        onView(withText(R.string.pref_title_font_size)).perform(click());
+        Espresso.pressBack();
+        onView(withText(R.string.pref_header_general)).perform(click());
+        onView(withText(R.string.pref_title_font_size)).check(matches(isDisplayed()));
+        String[] fontArray;
+        fontArray = InstrumentationRegistry.
+                getTargetContext().getResources().getStringArray(R.array.text_size);
+        for (String fontName :fontArray) {
+            onView(withText(R.string.pref_title_font_size)).perform(click());
+            onView(withText(fontName)).perform(click());
+        }
+        Espresso.pressBack();
+        onView(withText(R.string.pref_header_notifications)).perform(click());
+        Espresso.pressBack();
+        onView(withText(R.string.pref_header_data_sync)).perform(click());
     }
 }
